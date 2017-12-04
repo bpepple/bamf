5de9057ec6da4bf8290c9be0424c053edebcf92e
Brian Pepple <bdpepple@gmail.com>
2017-12-04 17:04:36 -0500

    Add some basic css & templates.
    * Switched css theme from Bulma to Bootstrap.

--- comics/urls.py
+++ comics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
 app_name = 'comics'
 urlpatterns = [
     path('series/', views.SeriesList.as_view(), name='comics_series_list')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
