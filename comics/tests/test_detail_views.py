70190ca1bd9ddddb132ecac4ae811b450da3c150
Brian Pepple <bdpepple@gmail.com>
2017-12-06 23:27:16 -0500

    Add tests for CreatorDetail view.

--- comics/tests/test_detail_views.py
+++ comics/tests/test_detail_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils.text import slugify
 
-from comics.models import (Publisher, Series)
+from comics.models import (Publisher, Series, Creator, Character, Team)
 
 
 HTML_OK_CODE = 200
@@ -50,3 +50,23 @@ class SeriesDetailViewTest(TestCase):
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTemplateUsed(resp, 'comics/series_detail.html')
+
+
+class CreatorDetailViewTest(TestCase):
+
+    def setUp(self):
+        self.creator = Creator.objects.create(
+            name='Jack Kirby',
+            slug='jack-kirby',
+            cvid=4321)
+
+    def test_view_url_accessible_by_name(self):
+        url = reverse('creator:detail', args=(self.creator.slug,))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, HTML_OK_CODE)
+
+    def test_view_uses_correct_template(self):
+        url = reverse('creator:detail', args=(self.creator.slug,))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, HTML_OK_CODE)
+        self.assertTemplateUsed(resp, 'comics/creator_detail.html')
