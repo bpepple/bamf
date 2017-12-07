202227aee0683e11d0fd55c211a78b4326960dee
Brian Pepple <bdpepple@gmail.com>
2017-12-06 23:53:49 -0500

    Use better paginate variable names.

--- comics/tests/test_list_views.py
+++ comics/tests/test_list_views.py
@@ -5,9 +5,9 @@ from comics.models import (Publisher, Series, Creator, Character, Team)
 
 
 HTML_OK_CODE = 200
-PAGINATE_VALUE = 28
-PAGINATE_TEST = 35
-PAGINATE_REMAINDER = 7
+PAGINATE_DEFAULT_VAL = 28
+PAGINATE_TEST_VAL = 35
+PAGINATE_DIFF_VAL = (PAGINATE_TEST_VAL - PAGINATE_DEFAULT_VAL)
 
 
 class PublisherListViewTest(TestCase):
@@ -15,7 +15,7 @@ class PublisherListViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Create 35 test publishers for pagination tests
-        number_of_publishers = PAGINATE_TEST
+        number_of_publishers = PAGINATE_TEST_VAL
         for pub_num in range(number_of_publishers):
             Publisher.objects.create(
                 name='Publisher %s' % pub_num,
@@ -39,7 +39,8 @@ class PublisherListViewTest(TestCase):
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['publisher_list']) == PAGINATE_VALUE)
+        self.assertTrue(
+            len(resp.context['publisher_list']) == PAGINATE_DEFAULT_VAL)
 
     def test_lists_all_publishers(self):
         # Get second page and confirm it has (exactly) remaining 7 items
@@ -48,7 +49,7 @@ class PublisherListViewTest(TestCase):
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(
-            len(resp.context['publisher_list']) == PAGINATE_REMAINDER)
+            len(resp.context['publisher_list']) == PAGINATE_DIFF_VAL)
 
 
 class SeriesListViewTest(TestCase):
@@ -58,7 +59,7 @@ class SeriesListViewTest(TestCase):
         pub, p_create = Publisher.objects.get_or_create(
             name='Marvel',
             slug='marvel')
-        number_of_series = PAGINATE_TEST
+        number_of_series = PAGINATE_TEST_VAL
         for ser_num in range(number_of_series):
             Series.objects.create(
                 name='Series %s' % ser_num,
@@ -84,21 +85,22 @@ class SeriesListViewTest(TestCase):
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['series_list']) == PAGINATE_VALUE)
+        self.assertTrue(
+            len(resp.context['series_list']) == PAGINATE_DEFAULT_VAL)
 
     def test_lists_all_series(self):
         resp = self.client.get(reverse('series:list') + '?page=2')
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['series_list']) == PAGINATE_REMAINDER)
+        self.assertTrue(len(resp.context['series_list']) == PAGINATE_DIFF_VAL)
 
 
 class CreatorListViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        number_of_creators = PAGINATE_TEST
+        number_of_creators = PAGINATE_TEST_VAL
         for creator in range(number_of_creators):
             Creator.objects.create(
                 name='Creator %s' % creator,
@@ -123,7 +125,8 @@ class CreatorListViewTest(TestCase):
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['creator_list']) == PAGINATE_VALUE)
+        self.assertTrue(
+            len(resp.context['creator_list']) == PAGINATE_DEFAULT_VAL)
 
     def test_lists_all_creators(self):
         resp = self.client.get(reverse('creator:list') + '?page=2')
@@ -131,14 +134,14 @@ class CreatorListViewTest(TestCase):
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(
-            len(resp.context['creator_list']) == PAGINATE_REMAINDER)
+            len(resp.context['creator_list']) == PAGINATE_DIFF_VAL)
 
 
 class CharacterListViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        number_of_characters = PAGINATE_TEST
+        number_of_characters = PAGINATE_TEST_VAL
         for character in range(number_of_characters):
             Character.objects.create(
                 name='Character %s' % character,
@@ -163,7 +166,8 @@ class CharacterListViewTest(TestCase):
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['character_list']) == PAGINATE_VALUE)
+        self.assertTrue(
+            len(resp.context['character_list']) == PAGINATE_DEFAULT_VAL)
 
     def test_lists_all_characters(self):
         resp = self.client.get(reverse('character:list') + '?page=2')
@@ -171,14 +175,14 @@ class CharacterListViewTest(TestCase):
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(
-            len(resp.context['character_list']) == PAGINATE_REMAINDER)
+            len(resp.context['character_list']) == PAGINATE_DIFF_VAL)
 
 
 class TeamListViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        number_of_teams = PAGINATE_TEST
+        number_of_teams = PAGINATE_TEST_VAL
         for team in range(number_of_teams):
             Team.objects.create(
                 name='Team %s' % team,
@@ -203,7 +207,7 @@ class TeamListViewTest(TestCase):
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['team_list']) == PAGINATE_VALUE)
+        self.assertTrue(len(resp.context['team_list']) == PAGINATE_DEFAULT_VAL)
 
     def test_lists_all_teams(self):
         resp = self.client.get(reverse('team:list') + '?page=2')
@@ -211,4 +215,4 @@ class TeamListViewTest(TestCase):
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(
-            len(resp.context['team_list']) == PAGINATE_REMAINDER)
+            len(resp.context['team_list']) == PAGINATE_DIFF_VAL)
