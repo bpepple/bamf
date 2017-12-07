eb2897fc2fa1d06fd5cd36634616f7022ce3e457
Brian Pepple <bdpepple@gmail.com>
2017-12-06 23:23:26 -0500

    Add tests for SeriesDetail view.

--- comics/tests/test_detail_views.py
+++ comics/tests/test_detail_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils.text import slugify
 
-from comics.models import (Publisher)
+from comics.models import (Publisher, Series)
 
 
 HTML_OK_CODE = 200
@@ -10,20 +10,43 @@ HTML_OK_CODE = 200
 
 class PublisherDetailViewTest(TestCase):
 
-    @classmethod
-    def setUpTestData(self):
-        self.name = 'DC Comics'
-        self.slug = slugify(self.name)
-        Publisher.objects.create(name=self.name,
-                                 slug=self.slug)
+    def setUp(self):
+        self.publisher = Publisher.objects.create(
+            name='DC Comics',
+            slug='dc-comics')
 
     def test_view_url_accessible_by_name(self):
-        url = reverse('publisher:detail', args=(self.slug,))
+        url = reverse('publisher:detail', args=(self.publisher.slug,))
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, HTML_OK_CODE)
 
     def test_view_uses_correct_template(self):
-        url = reverse('publisher:detail', args=(self.slug,))
+        url = reverse('publisher:detail', args=(self.publisher.slug,))
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTemplateUsed(resp, 'comics/publisher_detail.html')
+
+
+class SeriesDetailViewTest(TestCase):
+
+    def setUp(self):
+        pub, p_create = Publisher.objects.get_or_create(
+            name='DC Comics',
+            slug='dc-comics',)
+
+        self.series = Series.objects.create(
+            publisher=pub,
+            name='Superman',
+            slug='superman',
+            cvid=1234,)
+
+    def test_view_url_accessible_by_name(self):
+        url = reverse('series:detail', args=(self.series.slug,))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, HTML_OK_CODE)
+
+    def test_view_uses_correct_template(self):
+        url = reverse('series:detail', args=(self.series.slug,))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, HTML_OK_CODE)
+        self.assertTemplateUsed(resp, 'comics/series_detail.html')
