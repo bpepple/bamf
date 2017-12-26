2f6d9c14898ec566a726acf8d8563bb6c07e15d6
Brian Pepple <bdpepple@gmail.com>
2017-12-26 14:45:23 -0500

    Add some more information to the comiclist api.

--- comics/serializers.py
+++ comics/serializers.py
@@ -4,10 +4,12 @@ from comics.models import Issue
 
 
 class IssueListSerializer(serializers.HyperlinkedModelSerializer):
-    series = serializers.StringRelatedField(many=False)
+    arcs = serializers.StringRelatedField(many=True)
     characters = serializers.StringRelatedField(many=True)
+    series = serializers.StringRelatedField(many=False)
+    teams = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Issue
-        fields = ('id', 'series', 'name', 'number', 'date',
-                  'status', 'desc', 'characters', 'file')
+        fields = ('id', 'cvurl', 'series', 'name', 'number', 'date',
+                  'status', 'desc', 'characters', 'teams', 'arcs', 'file', 'mod_ts')
