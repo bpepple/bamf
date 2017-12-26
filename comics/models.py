32f89a774c900bb6f428e65f57f4f7b5ed07758f
Brian Pepple <bdpepple@gmail.com>
2017-12-26 13:09:30 -0500

    comics/models.py: Remove unused import

--- comics/models.py
+++ comics/models.py
@@ -2,7 +2,6 @@ import datetime
 
 from django.core.validators import RegexValidator
 from django.db import models
-from django.urls import reverse
 from multiselectfield import MultiSelectField
 from solo.models import SingletonModel
 
