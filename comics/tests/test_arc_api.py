3d161a7fd2087de351b0e62123e3ef1512c23fef
Brian Pepple <bdpepple@gmail.com>
2018-01-06 20:05:34 -0500

    Add test for the Arc REST API.

---
+++ comics/tests/test_arc_api.py
@@ -0,0 +1,50 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+
+from comics.models import Arc
+from comics.serializers import ArcSerializer
+
+
+# initialize the APIClient app
+client = Client()
+
+
+class GetAllArcsTest(TestCase):
+
+    def setUp(self):
+        Arc.objects.create(cvid='1234', cvurl='http://1.com',
+                           name='World War Hulk', slug='world-war-hulk')
+        Arc.objects.create(cvid='4321', cvurl='http://2.com',
+                           name='Final Crisis', slug='final-crisis')
+
+    def test_get_all_arcs(self):
+        # get API response
+        response = client.get(reverse('api:arc-list'))
+        # get data from db
+        arcs = Arc.objects.all()
+        serializer = ArcSerializer(arcs, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetSingleArcTest(TestCase):
+
+    def setUp(self):
+        self.hulk = Arc.objects.create(
+            cvid='1234', cvurl='http://1.com', name='World War Hulk', slug='world-war-hulk')
+        self.crisis = Arc.objects.create(
+            cvid='4321', cvurl='http://2.com', name='Final Crisis', slug='final-crisis')
+
+    def test_get_valid_single_arc(self):
+        response = client.get(
+            reverse('api:arc-detail', kwargs={'slug': self.hulk.slug}))
+        arc = Arc.objects.get(slug=self.hulk.slug)
+        serializer = ArcSerializer(arc)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_publisher(self):
+        response = client.get(
+            reverse('api:arc-detail', kwargs={'slug': 'civil-war'}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
