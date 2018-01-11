fba9a25045b9a33ff8a1f8194e7771f1184c8d32
Brian Pepple <bdpepple@gmail.com>
2018-01-11 16:14:23 -0500

    Change the db backend to PostgreSQL. Fixes #19 & Fixes #18.

--- bamf/settings.py
+++ bamf/settings.py
@@ -126,8 +126,13 @@ WSGI_APPLICATION = 'bamf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bamfproject',
+        'USER': 'bpepple',
+        'PASSWORD': 'bamfpassword',
+        'HOST': '',
+        'PORT': '',
+
     }
 }
 
