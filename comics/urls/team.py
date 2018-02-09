d27b77a3d6915e36047a1a10d639f5890652283e
Brian Pepple <bdpepple@gmail.com>
2018-02-09 18:10:21 -0500

    Fix SearchTeamList view's pagination.

--- comics/urls/team.py
+++ comics/urls/team.py
@@ -7,9 +7,9 @@ from comics.views.team import TeamDetail, TeamList, SearchTeamList
 
 app_name = 'team'
 urlpatterns = [
-    path('', TeamList.as_view(), name='list'),
+    path('page<int:page>/', TeamList.as_view(), name='list'),
     path('<slug:slug>/', TeamDetail.as_view(), name='detail'),
-    re_path(r'^search/?$', SearchTeamList.as_view(), name='search'),
+    re_path(r'^search/(?:page(?P<page>\d+)/)?$', SearchTeamList.as_view(), name='search'),
 ]
 
 if settings.DEBUG:
