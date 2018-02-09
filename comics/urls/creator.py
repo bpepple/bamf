9baa75c312d7c7199c45b6545f9cf2b362f2a232
Brian Pepple <bdpepple@gmail.com>
2018-02-09 18:03:52 -0500

    Fix SearchCreatorList view's pagination.

--- comics/urls/creator.py
+++ comics/urls/creator.py
@@ -7,9 +7,9 @@ from comics.views.creator import CreatorDetail, CreatorList, SearchCreatorList
 
 app_name = 'creator'
 urlpatterns = [
-    path('', CreatorList.as_view(), name='list'),
+    path('page<int:page>/', CreatorList.as_view(), name='list'),
     path('<slug:slug>/', CreatorDetail.as_view(), name='detail'),
-    re_path(r'^search/?$', SearchCreatorList.as_view(), name='search'),
+    re_path(r'^search/(?:page(?P<page>\d+)/)?$', SearchCreatorList.as_view(), name='search'),
 ]
 
 if settings.DEBUG:
