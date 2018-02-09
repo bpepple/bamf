661e190ad3338b479f81c987e23409218c785900
Brian Pepple <bdpepple@gmail.com>
2018-02-09 18:32:18 -0500

    Fix TeamListView test for changes made for search pagination.

--- comics/tests/test_views_list.py
+++ comics/tests/test_views_list.py
@@ -190,27 +190,27 @@ class TeamListViewTest(TestCase):
                 cvid=team)
 
     def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('/team/')
+        resp = self.client.get('/team/page1/')
         self.assertEqual(resp.status_code, HTML_OK_CODE)
 
     def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse('team:list'))
+        resp = self.client.get(reverse('team:list', args=(1,)))
         self.assertEqual(resp.status_code, HTML_OK_CODE)
 
     def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('team:list'))
+        resp = self.client.get(reverse('team:list', args=(1,)))
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTemplateUsed(resp, 'comics/team_list.html')
 
     def test_pagination_is_twenty_eight(self):
-        resp = self.client.get(reverse('team:list'))
+        resp = self.client.get(reverse('team:list', args=(1,)))
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(len(resp.context['team_list']) == PAGINATE_DEFAULT_VAL)
 
     def test_lists_all_teams(self):
-        resp = self.client.get(reverse('team:list') + '?page=2')
+        resp = self.client.get(reverse('team:list', args=(2,)))
         self.assertEqual(resp.status_code, HTML_OK_CODE)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
