39e4d4dd8ed0094af4ab00a68fc9d2ecd89a64a6
Brian Pepple <bdpepple@gmail.com>
2018-02-17 15:31:20 -0500

    Require users to login to use the importer.

--- comics/views/importer.py
+++ comics/views/importer.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 
 from comics.tasks import import_comic_files_task
 
 
+@login_required
 def importer(request):
     import_comic_files_task()
     return HttpResponseRedirect('/')
