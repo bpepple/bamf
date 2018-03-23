140cb48d7979228321bf9e3d17f6919e0c54d9c6
Brian Pepple <bdpepple@gmail.com>
2018-03-23 17:29:56 -0400

    Add authentication to the creator api tests.

--- comics/tests/test_api_creator.py
+++ comics/tests/test_api_creator.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -6,28 +7,51 @@ from comics.models import Creator
 from comics.serializers import CreatorSerializer
 
 
-class GetAllCreatorsTest(TestCase):
+class TestCaseBase(TestCase):
+
+    def _create_user(self):
+        user = User.objects.create(username='brian')
+        user.set_password('1234')
+        user.save()
+
+        return user
+
+    def _client_login(self):
+        self.client.login(username='brian', password='1234')
+
+
+class GetAllCreatorsTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
+        cls._create_user(cls)
+
         Creator.objects.create(
             cvid='1234', cvurl='http://1.com', name='John Byrne', slug='john-byrne')
         Creator.objects.create(cvid='4321', cvurl='http://2.com',
                                name='Walter Simonson', slug='walter-simonson')
+
+    def setUp(self):
+        self._client_login()
 
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:creator-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
 
-class GetSingleCreatorTest(TestCase):
+class GetSingleCreatorTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
+        cls._create_user(cls)
+
         cls.john = Creator.objects.create(
             cvid='1234', cvurl='http://1.com', name='John Byrne', slug='john-byrne')
         cls.walter = Creator.objects.create(
             cvid='4321', cvurl='http://2.com', name='Walter Simonson', slug='walter-simonson')
+
+    def setUp(self):
+        self._client_login()
 
     def test_get_valid_single_creator(self):
         response = self.client.get(
