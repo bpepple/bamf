ced76cb7d70d48ba009ff17d7af978baf7c7c48f
Brian Pepple <bdpepple@gmail.com>
2018-03-23 17:23:26 -0400

    Add authentication to the character api tests.

--- comics/tests/test_api_character.py
+++ comics/tests/test_api_character.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -6,29 +7,52 @@ from comics.models import Character
 from comics.serializers import CharacterSerializer
 
 
-class GetAllCharactersTest(TestCase):
+class TestCaseBase(TestCase):
+
+    def _create_user(self):
+        user = User.objects.create(username='brian')
+        user.set_password('1234')
+        user.save()
+
+        return user
+
+    def _client_login(self):
+        self.client.login(username='brian', password='1234')
+
+
+class GetAllCharactersTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
+        cls._create_user(cls)
+
         for character in range(105):
             Character.objects.create(
                 name='Character %s' % character,
                 slug='character-%s' % character,
                 cvid=character)
 
+    def setUp(self):
+        self._client_login()
+
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:character-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
 
-class GetSingleCharacterTest(TestCase):
+class GetSingleCharacterTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
+        cls._create_user(cls)
+
         cls.superman = Character.objects.create(
             cvid='1234', cvurl='http://1.com', name='Superman', slug='superman')
         cls.batman = Character.objects.create(
             cvid='4321', cvurl='http://2.com', name='Batman', slug='batman')
+
+    def setUp(self):
+        self._client_login()
 
     def test_get_valid_single_character(self):
         response = self.client.get(
